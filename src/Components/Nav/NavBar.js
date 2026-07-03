a6c15574edfbe366fa1ac8d6b5846071ceb0ea64
Tashi Woeser <tashiwoeser@Tashis-MacBook-Pro.local>
2026-07-03 16:43:43 -0700

    Polish responsive navbar and add prayer flag mobile menu

--- src/Components/Nav/NavBar.js
+++ src/Components/Nav/NavBar.js
@@ -8,16 +8,42 @@ const NavBar = () => {
     <header className="navbar-main">
       <Navbar expand="lg" className="taste-navbar">
         <Navbar.Brand as={Link} to="/" className="taste-brand">
-          <img
-            src="/images/taste-logo.png"
-            alt="Taste of the Himalayas logo"
-          />
+          <img src="/images/taste-logo.png" alt="Taste of the Himalayas logo" />
         </Navbar.Brand>
 
         <Navbar.Toggle
           aria-controls="taste-navbar-nav"
           className="taste-toggle"
-        />
+        >
+          <span className="prayer-hamburger" aria-hidden="true">
+            <span className="prayer-row prayer-row-one">
+              <span className="prayer-rope" />
+              <span className="prayer-flag flag-red" />
+              <span className="prayer-flag flag-yellow" />
+              <span className="prayer-flag flag-blue" />
+              <span className="prayer-flag flag-green" />
+              <span className="prayer-flag flag-orange" />
+            </span>
+
+            <span className="prayer-row prayer-row-two">
+              <span className="prayer-rope" />
+              <span className="prayer-flag flag-blue" />
+              <span className="prayer-flag flag-green" />
+              <span className="prayer-flag flag-red" />
+              <span className="prayer-flag flag-yellow" />
+              <span className="prayer-flag flag-blue" />
+            </span>
+
+            <span className="prayer-row prayer-row-three">
+              <span className="prayer-rope" />
+              <span className="prayer-flag flag-yellow" />
+              <span className="prayer-flag flag-red" />
+              <span className="prayer-flag flag-green" />
+              <span className="prayer-flag flag-blue" />
+              <span className="prayer-flag flag-orange" />
+            </span>
+          </span>
+        </Navbar.Toggle>
 
         <Navbar.Collapse id="taste-navbar-nav">
           <Nav className="taste-links">
